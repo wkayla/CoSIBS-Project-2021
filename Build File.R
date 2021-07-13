28c35c570ceec9b6fa954109f1a42b1301ff7594
Kayla Williamson <kayla.3.williamson@cuanschutz.edu>
2021-07-13 09:16:42 -0600

    Add readme and build file updates

--- Build File.R
+++ Build File.R
@@ -1,10 +1,10 @@
 library(plyr)
 library(tidyverse)
 
-path<-"C:/Users/wkayla/OneDrive - The University of Colorado Denver/COSIBS Project 2021/Data/"
+path<-"C:/Users/wkayla/OneDrive - The University of Colorado Denver/COSIBS Project 2021/Data/" #update this for where the data is stored at in your computer
 
 #Load in files
-clinical=read.csv(paste(path,"MEEP_DataForR_format_delete PHI.csv",sep=""), header = T)
+clinical=read.csv(paste(path,"MEEP_DataForR_format_delete PHI.csv",sep=""), header = T) #read.csv is a part of a suite of "read" functions. In this case Iuse all of the defaults except header which I update to True because all of our variable names are named in the CSV file
 clinical_carpe=read.csv(paste(path,"Copy of CarpeData delete PHI.csv",sep=""), header = T)
 metabolites=read.csv(paste(path,"metabolites raw text removed.csv",sep=""),header=T)
 nmr_carpe=read.csv(paste(path,"NMR carpe samples sheet1 updated.csv",sep=""),header=T)
@@ -20,24 +20,32 @@ clinical_carpe$study_id=paste(clinical_carpe$study_id,"Case",sep="")
 clinical_full=rbind.fill(clinical,clinical_carpe)
 
 #merge the link file and the urine samples
-colnames(nmr_carpe)=c("study_id","Sample.ID","Volume","Timepoint","Shipped","NMR","AIM")
+
+nmr_carpe=nmr_carpe%>%
+  mutate(Sample=ifelse(Sample=="Disharge","Discharge",as.character(Sample)))%>%
+  distinct(Study.ID,Sample,.keep_all=T)
+
+colnames(nmr_carpe)=c("study_id","Sample.ID","Volume.x","Timepoint","Shipped","NMR","AIM")
 nmr_carpe$Case="Case"
-colnames(nmr_meep)=c("study_id","Timepoint","Sample.ID","Volume","Date.Time")
+colnames(nmr_meep)=c("study_id","Timepoint","Sample.ID","Volume.y","Date.Time")
 nmr_meep$Case="Control"
 
 samples=rbind.fill(nmr_carpe,nmr_meep)
 
 samples=samples%>%
           filter(Timepoint!="T3" & Timepoint != "T3 ")%>%
-          mutate(Timepoint=ifelse(Timepoint=="T1" | 
+          mutate(Timepoint=ifelse(Timepoint=="T1" |
                                   Timepoint=="T1 " |
                                   Timepoint=="Enrollment","Enrollment","Discharge"))
 
 
 samples_link=samples%>%
-                inner_join(link_file,by=c("Sample.ID"="Study.ID"))%>%
-                inner_join(metabolites,by=c("New.CARPEDIEM.NOESY.NMR.exp.no..2"="Experiment.No."))%>%
+                full_join(link_file,by=c("Sample.ID"="Study.ID"))%>%
+                full_join(metabolites,by=c("New.CARPEDIEM.NOESY.NMR.exp.no..2"="Experiment.No."))%>%
                 dplyr::mutate(study_id=as.character(study_id),
                        study_id=paste0(study_id,Case,sep=""))
 
 clinical_metabolites=inner_join(samples_link,clinical_full)
+
+
+
